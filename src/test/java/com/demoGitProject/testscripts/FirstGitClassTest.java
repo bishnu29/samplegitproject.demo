49d9acba7c539f2578383d4999798d140e6558a5
bishnu <bishnu.infanion@gmail.com>
2016-01-18 18:59:37 +0530

    initial commit

---
+++ src/test/java/com/demoGitProject/testscripts/FirstGitClassTest.java
@@ -0,0 +1,13 @@
+package com.demoGitProject.testscripts;
+
+import org.testng.annotations.Test;
+
+public class FirstGitClassTest {
+
+	@Test
+	public void testTest(){
+		
+		System.out.println("1st Git project");
+	}
+	
+}
